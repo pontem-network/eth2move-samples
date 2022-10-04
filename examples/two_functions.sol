b69ef323f7941ac4cbee740773d5b520c0020675
vladimirovmm <vladimirov.m.m@ya.ru>
2022-10-04 18:52:13 +0500

    fix

--- examples/two_functions.sol
+++ examples/two_functions.sol
@@ -3,18 +3,11 @@
 pragma solidity ^0.8.10;
 
 contract TwoFunctions {
-    // # panic() !panic
     function panic() pure public {
         assert(false);
     }
 
-    // # do_nothing()
-    // # do_nothing(123) void !panic
     function do_nothing() public {}
 
-    // # boo(true)
-    // # boo(false)
-    // # boo(123) !panic
-    function boo(bool s) public {
-    }
+    function boo(bool s) public {}
 }
